ad6fbffdcc1470eb6b5c4a171dcdfd2d1a39fa17
Iago Rodrigues <yagomello16@gmail.com>
2023-05-09 23:38:24 -0300

    feat: add meals list and meal description screens

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category.dart';
+import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pick your category'),
-      ),
+      appBar: AppBar(title: const Text('Pick your category')),
       body: GridView(
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
@@ -21,8 +36,8 @@ class CategoriesScreen extends StatelessWidget {
           children: [
             for (final category in availableCategories)
               CategoryGridItem(
-                key: ValueKey(category.id),
                 category: category,
+                onSelectCategory: () => _selectCategory(context, category),
               ),
           ]),
     );
